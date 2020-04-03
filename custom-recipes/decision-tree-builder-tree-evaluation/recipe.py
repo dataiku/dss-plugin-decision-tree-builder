194eecbba0289b6269be2658d6fbcf0a3fe12565
Agathe Guillemot <agathe.guillemot@dataiku.com>
2020-04-03 19:07:24 +0200

    Enhancements on the recipes

--- custom-recipes/decision-tree-builder-tree-evaluation/recipe.py
+++ custom-recipes/decision-tree-builder-tree-evaluation/recipe.py
@@ -1,61 +1,54 @@
 from dataiku.customrecipe import get_input_names_for_role, get_output_names_for_role, get_recipe_config
-import pandas as pd
-from dku_idtb_decision_tree.tree import Tree
-from dku_idtb_scoring.score import score, write_with_schema
+from dku_idtb_decision_tree.tree import ScoringTree
+from dku_idtb_scoring.score import add_scoring_columns, get_scored_df_schema, get_metric_df_schema
 from dku_idtb_compatibility.utils import safe_str
 from dataiku.doctor.prediction.reg_evaluation_recipe import compute_multiclass_metrics, compute_binary_classification_metrics
 
-input_dataset = dataiku.Dataset(get_input_names_for_role("input_dataset")[0])
-scored_dataset = dataiku.Dataset(get_output_names_for_role("scored_dataset")[0])
-metrics_dataset = dataiku.Dataset(get_output_names_for_role("metrics_dataset")[0])
+input_dataset = dataiku.Dataset(get_input_names_for_role("inputDataset")[0])
 folder = dataiku.Folder(get_input_names_for_role("folder")[0])
-chunk_size_param = get_recipe_config()["chunk_size"]
+scored_dataset = dataiku.Dataset(get_output_names_for_role("scoredDataset")[0])
+metrics_dataset = dataiku.Dataset(get_output_names_for_role("metricsDataset")[0])
+recipe_config = get_recipe_config()
 
 try:
-    tree = folder.read_json(get_recipe_config()["tree_file"])
+    tree_dict = folder.read_json(recipe_config["treeFile"])
 except ValueError:
-    raise Exception("No tree file named " + get_recipe_config()["tree_file"])
+    raise Exception("No tree file named " + recipe_config["treeFile"])
 
-tree["df"] = input_dataset.get_dataframe()
-tree = Tree(**tree)
+tree = ScoringTree(tree_dict["target"], tree_dict["target_values"], tree_dict["nodes"], tree_dict["features"])
 
-scored_df = score(tree, input_dataset, chunk_size_param, True)
+columns = recipe_config["inputColumns"] if recipe_config["keepSomeColumns"] else None
+scored_dataset.write_schema(get_scored_df_schema(tree, input_dataset, columns, recipe_config["outputProbabilities"], True, recipe_config["checkPrediction"]))
+input_dataframe = input_dataset.get_dataframe()
+input_dataframe.loc[:, tree.target] = input_dataframe.loc[:, tree.target].apply(safe_str)
+add_scoring_columns(tree, input_dataframe, True, True, recipe_config["checkPrediction"])
+prediction_col_not_na = ~input_dataframe.prediction.isna()
 
-target_mapping = {safe_str(label): index for index, label in enumerate(tree.target_values)}
-scored_df_nona = scored_df.dropna(subset=["prediction"])
-y_actual, y_pred = scored_df_nona[tree.target], scored_df_nona.prediction
-y_actual = y_actual.map(lambda t: int(target_mapping[safe_str(t)]))
-y_pred = y_pred.map(lambda t: int(target_mapping[safe_str(t)]))
-
-if len(tree.target_values) > 2:
-    compute_metrics = compute_multiclass_metrics
-    metrics = ["precision", "recall", "accuracy", "mrocAUC", "logLoss", "hammingLoss", "mcalibrationLoss"]
-else:
-    compute_metrics = compute_binary_classification_metrics
-    metrics = ["precision", "recall", "accuracy", "auc",  "hammingLoss", "logLoss", "calibrationLoss"]
-
-sorted_classes = sorted(target_mapping, key=lambda label: target_mapping[label])
-
-if get_recipe_config()["probabilities"]:
-    probas = scored_df_nona[["proba_" + safe_str(label) for label in sorted_classes]]
-else:
-    probas = pd.DataFrame()
-    for value in sorted_classes:
-        probas["proba_" + value] = scored_df_nona.pop("proba_" + safe_str(value))
-
-metrics_dict = compute_metrics({"metrics": {"evaluationMetric": None, "liftPoint": 0.4}}, y_actual, y_pred, probas.values)
-schema_metrics = []
-for metric in metrics:
-    if metric == "mcalibrationLoss":
-        metric = "calibrationLoss"
-    if metric == "mrocAUC":
-        metric = "auc"
-    if get_recipe_config()["filterMetrics"] and not get_recipe_config()[metric]:
-        metrics_dict.pop(metric, "None")
+if not recipe_config["outputProbabilities"]:
+    import pandas as pd
+    probas_df = pd.DataFrame()
+    for target_value in tree.target_values:
+        probas_df["proba_" + target_value] = input_dataframe.pop("proba_" + safe_str(target_value))[prediction_col_not_na]
+with scored_dataset.get_writer() as writer:
+    if columns is None:
+            writer.write_dataframe(input_dataframe)
     else:
-        schema_metrics.append({"type": "float", "name": metric})
+        writer.write_dataframe(input_dataframe[columns])
 
-write_with_schema(tree, input_dataset, scored_dataset, scored_df_nona, get_recipe_config()["probabilities"], True)
-metrics_dataset.write_schema(schema_metrics)
+input_dataframe = input_dataframe[prediction_col_not_na]
+if recipe_config["outputProbabilities"]:
+    probas_df = input_dataframe[["proba_" + safe_str(target_value) for target_value in tree.target_values]]
+target_mapping = {safe_str(label): index for index, label in enumerate(tree.target_values)}
+y_pred = input_dataframe["prediction"].map(lambda t: int(target_mapping[safe_str(t)]))
+y_actual = input_dataframe[tree.target].map(lambda t: int(target_mapping[safe_str(t)]))
+
+if len(tree.target_values) == 2:
+    metrics_dict = compute_binary_classification_metrics({"metrics": {"evaluationMetric": None, "liftPoint": 0.4}}, y_actual, y_pred, probas_df.values)
+    metrics = ["precision", "recall", "accuracy", "auc",  "hammingLoss", "logLoss", "calibrationLoss"]
+else:
+    metrics_dict = compute_multiclass_metrics({"metrics": {"evaluationMetric": None, "liftPoint": 0.4}}, y_actual, y_pred, probas_df.values)
+    metrics = ["precision", "recall", "accuracy", "mrocAUC", "logLoss", "hammingLoss", "mcalibrationLoss"]
+
+metrics_dataset.write_schema(get_metric_df_schema(metrics_dict, metrics, recipe_config))
 with metrics_dataset.get_writer() as writer:
     writer.write_row_dict(metrics_dict)
